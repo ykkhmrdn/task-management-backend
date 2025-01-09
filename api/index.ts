2a6e596d736f25f61bec449e2fb6a657e0f1a1b7
ykkhmrdn <ykkhmrdn@gmail.com>
2025-01-09 12:44:10 +0700

    refactor: restructure project to move API entry point and update configurations

--- api/index.ts
+++ api/index.ts
@@ -2,7 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
-import taskRoutes from "./routes/taskRoutes";
+import taskRoutes from "../src/routes/taskRoutes";
 
 dotenv.config();
 
